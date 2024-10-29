1edbec8d100c74367314bb1939f2f2b9d34ce514
Bernard Ian Santella <santellabernardian@gmail.com>
2024-10-29 14:42:10 +0400

    Update Exercise 4.py

--- Exercise 4.py
+++ Exercise 4.py
@@ -3,14 +3,14 @@ print("What is the capital of France")
 answer = (input("Please enter your answer:" ))              
 if answer.lower() >= 'Paris':
     print ('You are correct')
-else:
+else: 
     print("You are incorrect")
 
 print("What is the capital of Czech Republic")          
 answer2 = (input("Please enter your answer:" ))
 if answer2.lower() >= 'Prague':
     print ('You are correct')
-else:
+else: 
     print("You are incorrect")
 
 print("What is the capital of Bulgaria")              
