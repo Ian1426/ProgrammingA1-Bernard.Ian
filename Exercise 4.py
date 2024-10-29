64a2c48f08ebc36b2e3346769e6ee21e523adf6e
Bernard Ian Santella <santellabernardian@gmail.com>
2024-10-29 14:40:02 +0400

    Update
    exericse1-4

---
+++ Exercise 4.py
@@ -0,0 +1,77 @@
+## Exercise 4 
+print("What is the capital of France")                 
+answer = (input("Please enter your answer:" ))              
+if answer.lower() >= 'Paris':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Czech Republic")          
+answer2 = (input("Please enter your answer:" ))
+if answer2.lower() >= 'Prague':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Bulgaria")              
+answer3 = (input("Please enter your answer:" ))
+if answer3.lower() == 'Sofia':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Finland")
+answer4 = (input("Please enter your answer:" ))
+if answer4.lower() == 'Helsinki':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Germany")                 
+answer5 = (input("Please enter your answer:" ))
+if answer5.lower() == 'Berlin':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Greece")                 
+answer6 = (input("Please enter your answer:" ))
+if answer6.lower() == 'Athens':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Ireland")                 
+answer7 = (input("Please enter your answer:" ))
+if answer7.lower() == 'Dublin':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Italy")                   
+answer8 = (input("Please enter your answer:" ))
+if answer8.lower() == 'Rome':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Macedonia")               
+answer9 = (input("Please enter your answer:" ))
+if answer9.lower() >= 'Skopje':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Norway")                  
+answer10 = (input("Please enter your answer:" ))
+if answer10.lower() == 'Oslo':
+    print ('You are correct')
+else:
+    print("You are incorrect")
+
+print("What is the capital of Serbia")                  
+answer11 = (input("Please enter your answer:" ))
+if answer11.lower() == 'Belgrade':
+    print ('You are correct')
+else:
+    print("You are incorrect")
---
+++ Exercise3.py
@@ -0,0 +1,25 @@
+## Exercise 3 
+dictionary = {'Name' : 'Bernard Ian Santella', 'Age' : '18', 'Location' : 'Ilocos Norte, Philippines'}
+print(dictionary)
+
+name1 = input("Enter your first name: ")
+name2 = input("Enter your second name: ")
+hometown = input("Enter your hometown: ")
+
+while True: 
+   age = input("Enter your age: ")
+   if age.isdigit():
+       age = int(age) 
+       break
+   else:
+       print ("Please Enter a valid number for your age.")
+
+person_info = {
+ 'First Name' : name1,
+ 'Second Name' : name2,
+ 'Hometown' : hometown,
+'Age' : age
+}
+
+for key, value in person_info.items():
+      print (f"{key}: {value}")
