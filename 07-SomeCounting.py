64a299817e40b5171a7993eddf24b64774ae41b8
Bernard Ian Santella <santellabernardian@gmail.com>
2024-11-04 19:19:29 +0400

    Revisions

--- 07-SomeCounting.py
+++ 07-SomeCounting.py
@@ -3,7 +3,7 @@
 print("Counting up from 0 to 50:")
 for i in range(0, 51):
     print(i, end=" ")
-print("\n") 
+print("\n") # Using a new line to print the code
 #Write a loop that counts down from 50 to 0 in decrements of 1.
 print("Counting up from 50 to 0:")
 for i in range(50, -1, -1):
