f5e2a6c15d8803bf3d2ebe4ac69d1cbdd474d367
Bernard Ian Santella <santellabernardian@gmail.com>
2024-11-12 13:22:57 +0400

    Update
    Changed the variable i

--- 07-SomeCounting/07-SomeCounting.py
+++ 07-SomeCounting/07-SomeCounting.py
@@ -2,33 +2,33 @@
 #Printing a messaged counting up from 0 to 50 
 print("Counting up from 0 to 50:")
 #Using a loop to iterate from 0 to 50 
-for i in range(0, 51):
-    print(i, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
+for s in range(0, 51):
+    print(s, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
 #Printing a newline to separate this line from the next line
 print("\n")                         #Adds a new line after finishing the first count
 #Printing a messaged counting up from 50 to 0
 print("Counting up from 50 to 0:")
 #Using a loop to iterate from 50 to 0 
-for i in range(50, -1, -1):          
-    print(i, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
+for s in range(50, -1, -1):          
+    print(s, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
 #Printing a newline to separate this line from the second count
 print("\n")                          #Adds a new line after finishing the second count
 #Printing a messaged counting up from 30 to 50 
 print("Counting up from 30 to 50:")
 #Using a loop to iterate from 30 to 50 
-for i in range(30, 51):
-    print(i, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
+for s in range(30, 51):
+    print(s, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
 print("\n")                         #Adds a new line after finishing the third count
 #Printing a messaged counting up from 50 to 10 in decrements of 2
 print("Counting down from 50 to 10 in decrements of 2:")
 #Using a loop to iterate from 50 to 10 in decrements of 2, decreasing
-for i in range(50, 9, -2):
-    print(i, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
+for s in range(50, 9, -2):
+    print(s, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
 print("\n")                         #Adds a new line after finishing the fourth count
 #Printing a messaged counting up from 100 to 200 in decrements of 5
 print("Counting up from 100 to 200 in increments of 5:")
 #Using a loop to iterate from 100 to 200 in decrements of 2, increasing
-for i in range(100, 201, 5):
-    print(i, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
+for s in range(100, 201, 5):
+    print(s, end=" ")               #Using end=" " to keep the output on the same line with spaces between numbers
 #Printing the final line to ensure the code runs from the first line to the last line of code
 print() 
